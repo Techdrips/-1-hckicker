00c8eadd3a68673edad166bf13f80357742d3dd6
Techdrips <168418866+Techdrips@users.noreply.github.com>
2024-10-17 19:13:18 +1100

    sw.js

--- sw.js
+++ sw.js
@@ -28,3 +28,17 @@ self.addEventListener("fetch", event => {
         })
     );
 });
+
+navigator.serviceWorker.register('/repository-name/sw.js');
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
