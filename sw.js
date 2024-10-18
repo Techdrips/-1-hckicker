0591a4cac2e62edb74b9b3d897bcd043803f2fa2
Techdrips <168418866+Techdrips@users.noreply.github.com>
2024-10-18 11:02:33 +1100

    sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,23 @@
-const CACHE_NAME = "junk-clicker-cache-v1";
-const urlsToCache = [
-    "/-1-hckicker/",
-    "/-1-hckicker/index.html",
-    "/-1-hckicker/styles.css",
-    "/-1-hckicker/app.js",
-    "/-1-hckicker/dorito.png",
-    "/-1-hckicker/burger.png",
-    "/-1-hckicker/pizza.png",
-    "/-1-hckicker/fries.png",
-    "/-1-hckicker/icon-192x192.png",
-    "/-1-hckicker/icon-512x512.png"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Opened cache");
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('junk-clicker-cache').then(function(cache) {
+      return cache.addAll([
+        '/-1-hckicker/',
+        '/-1-hckicker/index.html',
+        '/-1-hckicker/manifest.json',
+        '/-1-hckicker/icon-192x192.png',
+        '/-1-hckicker/icon-512x512.png',
+        '/-1-hckicker/styles.css', // if you have styles
+        '/-1-hckicker/app.js' // if you have external JS
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
-
-navigator.serviceWorker.register('/repository-name/sw.js');
-
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
